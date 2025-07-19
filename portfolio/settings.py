331504ff83444e53a963f26fe988274f748fb0b0
gamer <openaixander@gmail.com>
2025-07-19 07:21:25 +0100

    This is my portfolio

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-!@#%$^&*()_+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['alameen-portfolio.onrender.com']
 
 
 # Application definition

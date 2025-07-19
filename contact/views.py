672925019685c9f8c7e259d88383304c897b3c20
gamer <openaixander@gmail.com>
2025-07-19 08:03:23 +0100

    This is my portfolio

--- contact/views.py
+++ contact/views.py
@@ -1,41 +1,35 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages  # Import Django's messaging framework
+from django.contrib import messages
 from .forms import ContactForm
 
+# Create your views here.
+
 def contact(request):
-    """
-    Handles the display and processing of the contact form.
-    """
-    # Check if the request method is POST, which means the form has been submitted.
+    """View to handle contact form submissions."""
+
+    # checks whether the request method is POST
     if request.method == 'POST':
-        # Create a form instance and populate it with data from the request.
+        # create a form instance with the submitted data
         form = ContactForm(request.POST)
-        
-        # Check if the form's data is valid according to the rules in our form class.
+
+
+        # checks if the form is valid
         if form.is_valid():
-            # If the form is valid, save the new ContactSubmission object to the database.
+            # save the form data to the database
             form.save()
-            
-            # Add a success message that will be displayed on the next page the user sees.
-            # This is a much better user experience than a simple alert.
+            # display a success message
             messages.success(request, 'Thank you for your message! I will get back to you soon.')
-            
-            # Redirect the user back to the same contact page.
-            # The page will now be clean, and the success message will appear.
-            return redirect('contact') # Make sure you have a URL named 'contact'
+            # redirect to the same page or another page
+            return redirect('contact:contact')
+
         else:
-            # If the form is not valid, add an error message.
-            messages.error(request, 'There was an error in your form. Please check the fields and try again.')
-    
-    # If the request method is GET (i.e., the user is just visiting the page),
-    # create a blank instance of the form.
+            # if the form is not valid, display an error message
+            messages.error(request, 'There was an error sending your message. Please try again.')
     else:
+        # if the request method is not POST, create an empty form
         form = ContactForm()
 
-    # Prepare the context dictionary to pass the form to the template.
     context = {
-        'form': form
+        'form': form,
     }
-    
-    # Render the contact page template with the form.
     return render(request, 'contact/contact_me.html', context)

a8cccb1f29f426af2887116512ed13429d2c0db1
gamer <openaixander@gmail.com>
2025-07-19 07:59:58 +0100

    This is my portfolio

--- myport/urls.py
+++ myport/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
 from . import views
 
-app_name = 'contact'
-
+app_name = 'myport'
 
 urlpatterns = [
-    path('', views.contact, name='contact'),
+    path('', views.index, name='index'),
+    path('projects/', views.projects, name='projects'),
+    path('about/', views.about, name='about'),
+    # Add more URL patterns as needed
 ]
